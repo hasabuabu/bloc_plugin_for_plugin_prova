59a9c1863ec2e23a42a50e962f90375bc61211c4
a21hasabuabu <a21hasabuabu@inspedralbes.cat>
2022-04-21 11:00:10 +0000

    Plugin block fet

---
+++ block_prova_list.php
@@ -0,0 +1,51 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Course list block.
+ *
+ * @package    block_course_list
+ * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+include_once($CFG->dirroot . '/course/lib.php');
+
+class block_prova_list extends block_list {
+    function init() {
+        $this->title = get_string('pluginname', 'block_prova_list');
+    }
+
+    function has_config() {
+        return true;
+    }
+
+    function get_content() {
+        global $CFG, $USER, $DB, $OUTPUT;
+
+        if($this->content !== NULL) {
+            return $this->content;
+        }
+
+        $this->content = new stdClass;
+        $this->content->items = array();
+        $this->content->items[] = "<a href=\"$CFG->wwwroot/report/prova/view.php?id=2\">".get_string('pluginname', 'block_prova_list').'</a> ...';
+        return $this->content;
+    }
+   
+}
+
+
